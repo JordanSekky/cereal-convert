f0ea8f7158e46d911d10a830bd7578d73f83e354
Jordan Sechler <jordansekky@gmail.com>
2021-12-29 19:02:21 -0500

    Restart only crashed threads.

--- src/main.rs
+++ src/main.rs
@@ -32,34 +32,43 @@ async fn main() -> Result<(), Error> {
 
     let pool = establish_connection_pool();
 
+    let cancel = tokio::spawn(signal::ctrl_c());
+    tokio::pin!(cancel);
+    let mut server = Box::pin(tokio::spawn(get_server_future(&pool)));
+    let mut check_for_new_chapters =
+        Box::pin(tokio::spawn(tasks::check_new_chap_loop(pool.clone())));
+    let mut send_notification =
+        Box::pin(tokio::spawn(tasks::send_notifications_loop(pool.clone())));
+
     loop {
-        let cancel = tokio::spawn(signal::ctrl_c());
-        let server = tokio::spawn(get_server_future(&pool));
-        let check_for_new_chapters = tokio::spawn(tasks::check_new_chap_loop(pool.clone()));
-        let send_notification = tokio::spawn(tasks::send_notifications_loop(pool.clone()));
         tokio::select! {
-        x = server => {
-            error!("API server thread failed. Restarting all threads.");
+        x = &mut server => {
+            error!("API server thread failed. Restarting the thread.");
             match x {
                 Ok(_) => error!("New chapter check returned OK. This should not be possible."),
                 Err(err) => error!(?err, "New chapter check has paniced. This should not be possible."),
             };
+            server.set(tokio::spawn(get_server_future(&pool)));
+
         },
-        x = check_for_new_chapters => {
+        x = &mut check_for_new_chapters => {
             error!("New chapter check thread failed. Restarting the thread.");
             match x {
                 Ok(_) => error!("New chapter check returned OK. This should not be possible."),
                 Err(err) => error!(?err, "New chapter check has paniced. This should not be possible."),
             };
+            check_for_new_chapters.set(tokio::spawn(tasks::check_new_chap_loop(pool.clone())));
+
         }
-        x = send_notification => {
+        x = &mut send_notification => {
             error!("Chapter notification thread failed. Restarting the thread.");
             match x {
                 Ok(_) => error!("Chapter notification thread returned OK. This should not be possible."),
                 Err(err) => error!(?err, "Chapter notification thread returned has paniced. This should not be possible."),
             };
+            send_notification.set(tokio::spawn(tasks::send_notifications_loop(pool.clone())));
         }
-        _ = cancel => { println!("Received exit signal, exiting."); break}
+        _ = &mut cancel => { println!("Received exit signal, exiting."); break}
         }
     }
     Ok(())
